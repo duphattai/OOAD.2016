a14b5e58f66a6328060ebfb2cffc80bbf9a0c3c8
duphattai <uitdptai@gmail.com>
2017-01-06 01:46:08 +0700

    done

--- CarManager/CarManager/Controllers/HomeController.cs
+++ CarManager/CarManager/Controllers/HomeController.cs
@@ -9,30 +9,10 @@ namespace CarManager.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IRoleService _roleService;
-        public HomeController(IRoleService roleService)
-        {
-            _roleService = roleService;
-        }
 
         public ActionResult Index()
         {
-            ViewBag.AllRoles = string.Join("; ", _roleService.GetAll().Select(o=>o.RoleName));
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return RedirectToAction("Index", "DashBoard", new { area = "Admin"});
         }
     }
 }
